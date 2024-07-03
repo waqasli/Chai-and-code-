ec1b3efe9749520fee6803bf1ee1813b504d3578
Waqas Ali <92053959+waqasli@users.noreply.github.com>
2024-07-03 02:05:12 +0000

    Datatypes

---
+++ 01lec/datatypes.js
@@ -0,0 +1,21 @@
+"Use strict"; //treat all js code as newer version
+//alert(3+3) we are using node js not browser
+
+console.log(3+
+    3) //code readablity should be high
+console.log("waqas")
+
+let name="waqas"
+let age=18
+let isLoggedIn=false
+let state;
+//number=> 2 power 53
+//bigint 
+//String=> ""
+//booleaan=> true/false
+//null=> Standalone value
+//undefined=>
+//symbol=>unique
+
+//object 
+console.log(typeof undefined)
